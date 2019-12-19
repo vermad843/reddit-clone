aa5babb6a8bc7ccb0b95321997ddb6513edbd45a
vermad843 <vishnuverma7524@gmail.com>
2019-12-20 03:38:31 +0530

    navbar and posts on the page

---
+++ README.md
@@ -0,0 +1,12 @@
+reddit clone :
+
+
+[x]vue init webpack reddit-clone
+
+[x]Install vue-router
+
+[x]cd reddit-clone
+
+[x] npm run dev
+
+
---
+++ src/components/Posts.vue
@@ -0,0 +1,19 @@
+ <template>
+  <div class="hello">
+     <h1>Posts</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Posts',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
