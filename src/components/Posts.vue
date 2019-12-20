dc9771028657e7ae67006962903af261c034a76c
vermad843 <vishnuverma7524@gmail.com>
2019-12-20 11:24:46 +0530

    adding some property like comments

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,11 +1,15 @@
  <template>
   <div class="hello mt-3">
       <ul class="list-unstyled">
-        <li v-for="post in posts" class="media">
-          <img class="mr-3" :src="post.data.thumbnail" alt="...">
+        <li v-for="post in posts" class="media m-3">
+          <img class="mr-3" :src="post.data.thumbnail" alt="">
           <div class="media-body">
-            <h5 class="mt-0 mb-1">List-based media object</h5>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            <h5 class="mt-0 mb-1">{{post.data.title}}</h5>
+            <p>
+              <h3 class = "text-danger">{{post.data.ups}}</h3>
+              <p>created {{post.data.created}}</p>
+              <span class="badge badge-pill badge-secondary">{{post.data.num_comments}} comments</span>
+            </p>
           </div>
         </li>
       </ul>
@@ -36,6 +40,5 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>
